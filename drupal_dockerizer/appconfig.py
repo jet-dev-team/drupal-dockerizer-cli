4f1b3acd014bd107f19ccc702a903a68390a8586
svicervlad <vlad.sadretdinov@jet.dev>
2021-04-02 08:25:52 +0300

    Fix versions

--- drupal_dockerizer/appconfig.py
+++ drupal_dockerizer/appconfig.py
@@ -23,7 +23,7 @@ class AppConfig:
         if not os.path.exists(self.config_file_path):
             self.data = {
                 "is_check_requirements_tools": False,
-                "version": "0.0.4",
+                "version": "0.0.5",
                 "instances": {},
             }
             self.save()
